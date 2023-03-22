ffe28d15b7ad1846690c4f8e80622869778b58ca
sormaxalex <128036375+sormaxalex@users.noreply.github.com>
2023-03-22 13:26:47 +0300

    v1

---
+++ WindowsFormsApp24/WindowsFormsApp24/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i=0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i==5000)
+            {
+                timer1.Stop();
+                Program.f2.Show();
+                this.Hide();
+            }
+            else
+            {
+                i += 100;
+                progressBar1.PerformStep();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp24/WindowsFormsApp24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp24
+{
+    static class Program
+    {
+        public static Form1 f1 = new Form1();
+        public static Form2 f2 = new Form2();
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(f1);
+        }
+    }
+}
